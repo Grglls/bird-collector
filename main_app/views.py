fa9793e071dc675b68acd66fecc21760d881f602
Greg Ells <gregells@gmail.com>
2023-12-18 18:54:49 +1300

    Lab 4 completed.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -17,6 +18,31 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
+    # Instantiate the FeedingForm to be rendered in detail.html:
+    feeding_form = FeedingForm()
     return render(request, 'birds/detail.html', {
-        'bird': bird
+        'bird': bird,
+        'feeding_form': feeding_form
     })
+
+def add_feeding(request, bird_id):
+    # Create a ModelForm instance using the data in request.POST:
+    form = FeedingForm(request.POST)
+    # Validate the form:
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
+
+class BirdCreate(CreateView):
+    model = Bird
+    fields = '__all__'
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = ['species', 'description', 'age']
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds'
